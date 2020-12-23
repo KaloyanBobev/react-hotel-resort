330b26752fb70e3085a9843a336235a344123e70
kaloyanbobev <kaloyanbobev@gmail.com>
2020-12-23 17:07:37 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ export default class Navbar extends Component {
         return (
             <nav className="navbar">
                 <div className="nav-center">
-                    <div className="nav=header">
+                    <div className="nav-header">
                         <Link to="/">
                             <img src={logo} alt="Beach Resort" />
                         </Link>
