6e76bf786d7dba3c521af359f2d4f3ca21a3195a
Kaloyan <kaloyanbobev@gmail.com>
2024-09-25 21:48:06 +0100

    Update FeaturedRooms.js

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -9,17 +9,17 @@ export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
 
   render() {
-    let { loadingMsg, featuredRooms: rooms } = this.context;
+    let { loading, featuredRooms: rooms } = this.context;
 
-    rooms = rooms.map((roomObj) => {
-      return <Room key={roomObj.id} room={roomObj} />;
+    rooms = rooms.map((room) => {
+      return <Room key={room.id} room={room} />;
     });
 
     return (
       <section className="featured-rooms">
         <Title title="featured rooms" />
         <div className="featured-rooms-center">
-          {loadingMsg ? <Loading /> : rooms}
+          {loading ? <Loading /> : rooms}
         </div>
       </section>
     );
